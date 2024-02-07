51c504d18e0c110fb00ce35a90ac1da003c4c34c
Erion <117570706+Erion14@users.noreply.github.com>
2024-02-07 15:16:06 +0100

    Time: 4 ms (100.00%) | Memory: 43.7 MB (70.14%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,20 +1,16 @@
 class Solution {
     public boolean isPalindrome(int x) {
-         if (x < 0 || (x % 10 == 0 && x != 0)) {
+         if (x < 0) {
             return false;
         }
         int n = x;
         int reversed = 0;
+        
         while(x != 0){
             int digit = x % 10;
             reversed = reversed * 10 + digit;
             x /= 10;
         }
-        if(reversed == n){
-            return true;
-        }
-        else{
-            return false;
-        }
+       return (reversed == n);
     }
 }
